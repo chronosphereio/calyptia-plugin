8f39dd1af5de6a878db98d1e5d1ba5d3605378b2
Nicolás Parada <nicolas@calyptia.com>
2022-04-27 22:20:53 -0400

    plugin: test

--- plugin/testdata/output/output.go
+++ plugin/testdata/output/output.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"context"
-	"log"
+	"fmt"
+	"os"
 	"time"
 
 	"github.com/fluent/fluent-bit-go/plugin"
@@ -14,11 +15,23 @@ func init() {
 
 type dummyPlugin struct{}
 
-func (plug dummyPlugin) Init(ctx context.Context, conf plugin.ConfigLoader) error { return nil }
+func (plug dummyPlugin) Init(ctx context.Context, conf plugin.ConfigLoader) error {
+	return nil
+}
 
 func (plug dummyPlugin) Flush(ctx context.Context, ch <-chan plugin.Message) error {
+	f, err := os.Create("/fluent-bit/etc/output.txt")
+	if err != nil {
+		return fmt.Errorf("could not open output.txt: %w", err)
+	}
+
+	defer f.Close()
+
 	for msg := range ch {
-		log.Printf("message=\"got record\" tag=%s time=%s record=%+v\n", msg.Tag(), msg.Time.Format(time.RFC3339), msg.Record)
+		_, err := fmt.Fprintf(f, "message=\"got record\" tag=%s time=%s record=%+v\n", msg.Tag(), msg.Time.Format(time.RFC3339), msg.Record)
+		if err != nil {
+			return fmt.Errorf("could not write to output.txt: %w", err)
+		}
 	}
 
 	return nil
