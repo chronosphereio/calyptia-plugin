6c70ea05617e9eacd437656835d721cbdb9dc0ae
Hiroshi Hatake <hatake@calyptia.com>
2022-05-31 20:33:06 +0900

    example: in_gdummy: Add CMetrics operation example
    Signed-off-by: Hiroshi Hatake <hatake@calyptia.com>

--- examples/in_gdummy/in_gdummy.go
+++ examples/in_gdummy/in_gdummy.go
@@ -4,14 +4,18 @@ package main
 #include <stdlib.h>
 */
 import "C"
-
 import (
 	"fmt"
 	"time"
 	"unsafe"
 
 	"github.com/calyptia/plugin/input"
+	"github.com/calyptia/cmetrics-go"
 )
+
+var cmt *cmetrics.Context
+var counter_success *cmetrics.Counter
+var counter_failure *cmetrics.Counter
 
 //export FLBPluginRegister
 func FLBPluginRegister(def unsafe.Pointer) int {
@@ -22,9 +26,27 @@ func FLBPluginRegister(def unsafe.Pointer) int {
 // (fluentbit will call this)
 // plugin (context) pointer to fluentbit context (state/ c code)
 func FLBPluginInit(plugin unsafe.Pointer) int {
+	var err error
 	// Example to retrieve an optional configuration parameter
 	param := input.FLBPluginConfigKey(plugin, "param")
 	fmt.Printf("[flb-go] plugin parameter = '%s'\n", param)
+
+	cmt, err = input.FLBPluginGetCMetricsContext(plugin)
+	if err != nil {
+		return input.FLB_ERROR
+	}
+	counter_success, err = cmt.CounterCreate("fluentbit", "input",
+		"operation_succeeded_total", "Total number of succeeded operations", []string{"plugin_name"})
+	if err != nil {
+		return input.FLB_ERROR
+	}
+
+	counter_failure, err = cmt.CounterCreate("fluentbit", "input",
+		"operation_failed_total", "Total number of failed operations", []string{"plugin_name"})
+	if err != nil {
+		return input.FLB_ERROR
+	}
+
 	return input.FLB_OK
 }
 
@@ -39,6 +61,10 @@ func FLBPluginInputCallback(data *unsafe.Pointer, size *C.size_t) int {
 	enc := input.NewEncoder()
 	packed, err := enc.Encode(entry)
 	if err != nil {
+		err = counter_failure.Inc(now, []string{"in_gdummy"})
+		if err != nil {
+			return input.FLB_ERROR
+		}
 		fmt.Println("Can't convert to msgpack:", message, err)
 		return input.FLB_ERROR
 	}
@@ -48,6 +74,11 @@ func FLBPluginInputCallback(data *unsafe.Pointer, size *C.size_t) int {
 	*size = C.size_t(length)
 	// For emitting interval adjustment.
 	time.Sleep(1000 * time.Millisecond)
+
+	err = counter_success.Inc(now, []string{"in_gdummy"})
+	if err != nil {
+		return input.FLB_ERROR
+	}
 
 	return input.FLB_OK
 }
