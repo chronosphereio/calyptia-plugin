4040a61de0fb4cdde8a62ded956383c705a2c86a
Phillip Whelan <phil@calyptia.com>
2023-08-29 15:10:57 -0400

    cshared: add regression test for ctrl+c blocking bug.
    Signed-off-by: Phillip Whelan <phil@calyptia.com>

---
+++ cshared_test.go
@@ -0,0 +1,40 @@
+package plugin
+
+import (
+	"context"
+	"testing"
+	"time"
+	"unsafe"
+)
+
+type testPluginInputCallback struct {
+}
+
+func (t testPluginInputCallback) Init(ctx context.Context, fbit *Fluentbit) error {
+	return nil
+}
+
+func (t testPluginInputCallback) Collect(ctx context.Context, ch chan<- Message) error {
+	return nil
+}
+
+func TestInputCallbackCtrlC(t *testing.T) {
+	theInput = testPluginInputCallback{}
+	cdone := make(chan bool)
+	timeout := time.NewTimer(1 * time.Second)
+	ptr := unsafe.Pointer(nil)
+
+	initWG.Done()
+	registerWG.Done()
+
+	go func() {
+		FLBPluginInputCallback(&ptr, nil)
+		cdone <- true
+	}()
+
+	select {
+	case <-cdone:
+	case <-timeout.C:
+		t.Fail()
+	}
+}
